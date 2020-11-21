a141a298fdc6005ff67fd46fa5451ca40768289d
kashpitan <kashpitan@googlemail.com>
2020-11-21 02:37:25 +0000

    socket implementation, connection to server and message sending functionality

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-// import axios from "axios";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Keyboard,
+} from "react-native";
+import io from "socket.io-client";
 
 const App = () => {
   const [messageInput, setMessageInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  var socket = io("http://192.168.0.62:8080", {
+    transports: ["websocket"],
+    jsonp: false,
+  });
+
+  componentDidMount = () => {};
   useEffect(() => {
-    fetch("http://192.168.0.62:8080/")
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log("Server Error");
-        console.log(err);
-      });
+    // fetch("http://192.168.0.62:8080/")
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log("Server Error");
+    //     console.log(err);
+    //   });
+
+    socket.connect();
+    socket.on("connect", () => {
+      console.log("connected to socket server");
+    });
+    socket.on("chat", (data) => {
+      console.log(data);
+    });
   }, []);
+
+  const sendMessage = () => {
+    socket.emit("chat", messageInput);
+    setMessageInput("");
+    Keyboard.dismiss();
+  };
 
   return (
     <>
@@ -28,10 +56,10 @@ const App = () => {
       <TextInput
         style={styles.messageBox}
         placeholder="enter a message..."
-        onChange={(text) => setMessageInput(text)}
+        onChangeText={(text) => setMessageInput(text)}
         value={messageInput}
       ></TextInput>
-      <Button title="Send"></Button>
+      <Button title="Send" onPress={() => sendMessage()}></Button>
     </>
   );
 };
