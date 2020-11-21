a5a5e076ac7158db9a5bc2683de2053be61b2fd4
kashpitan <kashpitan@googlemail.com>
2020-11-21 01:19:13 +0000

    initial commit: button and text box, testing api request to server (locally)

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+// import axios from "axios";
 
-export default function App() {
+const App = () => {
+  const [messageInput, setMessageInput] = useState("");
+  useEffect(() => {
+    fetch("http://192.168.0.62:8080/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("Server Error");
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Omon chat</Text>
+        <StatusBar style="auto" />
+      </View>
+      <TextInput
+        style={styles.messageBox}
+        placeholder="enter a message..."
+        onChange={(text) => setMessageInput(text)}
+        value={messageInput}
+      ></TextInput>
+      <Button title="Send"></Button>
+    </>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  messageBox: {
+    paddingTop: 10,
+    paddingBottom: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 20,
   },
 });
